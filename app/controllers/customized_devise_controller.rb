36c8155a8c6c2ee125965622eb91f9435b52690a
Ivan Zabrovskiy <loriowar@gmail.com>
2016-07-23 16:34:48 +0300

    New patent controller for devise
     - added custom parent controller for devise
     - removed .rvmrc

---
+++ app/controllers/customized_devise_controller.rb
@@ -0,0 +1,14 @@
+class CustomizedDeviseController < ApplicationController
+
+  ## Changes for using login or email in 'devise'
+
+protected
+
+  def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super # Use the default one
+    end
+  end
+end
